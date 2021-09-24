1702caba07142d1c3e2c64a3079cffd3fab27142
CatUnderTheLeaf <stacy.chaikovska@gmail.com>
2021-09-24 23:28:16 +0200

    added scrollView with listitems in each tab

--- menuGeneratorApp/main.py
+++ menuGeneratorApp/main.py
@@ -1,13 +1,19 @@
 from datetime import date, timedelta
 
 from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.tab import MDTabsBase
+from kivy.properties import StringProperty
+from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem, OneLineListItem, TwoLineAvatarListItem
+from kivymd.icon_definitions import md_icons
 
-class MenuGenerator(MDBoxLayout):
-    pass
+
+class ListItemWithCheckbox(OneLineAvatarIconListItem):
+    '''Custom list item.'''
+
+    icon = StringProperty("android")
+
+
 
 class Tab(MDFloatLayout, MDTabsBase):
     '''Class implementing content for a tab.'''
@@ -19,6 +25,17 @@ class MenuGeneratorApp(MDApp):
             day = sdate + timedelta(days=i)
             day_title = "\n{}, {} {}".format(day.strftime("%A"), day.day, day.strftime("%b"))
             self.root.ids.tabs.add_widget(Tab(title=day_title))
+        icons = list(md_icons.keys())
+        tabs = self.root.ids.tabs.get_tab_list()
+        for tab in tabs:
+            for i in range(3):
+                tab.tab.ids.scroll.add_widget(
+                    OneLineListItem(text=f"Meal {i+1}")
+                )
+                tab.tab.ids.scroll.add_widget(
+                    TwoLineAvatarListItem(text=f"Recipe {i+1}")
+                )
+            
 
     def on_tab_switch(self, instance_tabs, instance_tab, instance_tab_label, tab_text):
         '''Called when switching tabs.
@@ -28,8 +45,8 @@ class MenuGeneratorApp(MDApp):
         :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
         :param tab_text: text or name icon of tab;
         '''
-
-        instance_tab.ids.container.text = tab_text
+        pass
+        # instance_tab.ids.container.text = tab_text
     pass
     # def build(self):
     #     return MenuGenerator()
