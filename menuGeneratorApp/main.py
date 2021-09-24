db2fa1ff532dfaf83eaf0092cf679c0059fd6203
CatUnderTheLeaf <stacy.chaikovska@gmail.com>
2021-09-24 15:50:00 +0200

    KivyApp: add tabView

--- menuGeneratorApp/main.py
+++ menuGeneratorApp/main.py
@@ -1,15 +1,38 @@
+from datetime import date, timedelta
+
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.tab import MDTabsBase
 
-class MenuGenerator(BoxLayout):
+class MenuGenerator(MDBoxLayout):
     pass
 
-class MenuGeneratorApp(MDApp):
+class Tab(MDFloatLayout, MDTabsBase):
+    '''Class implementing content for a tab.'''
 
-    def build(self):
-        self.theme_cls.primary_palette = "Gray"
-        return MenuGenerator()
+class MenuGeneratorApp(MDApp):
+    def on_start(self):
+        sdate = date.today()
+        for i in range(10):
+            day = sdate + timedelta(days=i)
+            day_title = "\n{}, {} {}".format(day.strftime("%A"), day.day, day.strftime("%b"))
+            self.root.ids.tabs.add_widget(Tab(title=day_title))
+
+    def on_tab_switch(self, instance_tabs, instance_tab, instance_tab_label, tab_text):
+        '''Called when switching tabs.
+
+        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
+        :param instance_tab: <__main__.Tab object>;
+        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
+        :param tab_text: text or name icon of tab;
+        '''
+
+        instance_tab.ids.container.text = tab_text
+    pass
+    # def build(self):
+    #     return MenuGenerator()
 
 if __name__ == '__main__':
     MenuGeneratorApp().run()
