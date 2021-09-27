c83078d4762b45d81bb55ff913a3a650e2d1fb7c
CatUnderTheLeaf <stacy.chaikovska@gmail.com>
2021-09-27 15:16:55 +0200

    add expansion panels

--- menuGeneratorApp/main.py
+++ menuGeneratorApp/main.py
@@ -2,10 +2,14 @@ from datetime import date, timedelta
 
 from kivymd.app import MDApp
 from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.tab import MDTabsBase
 from kivy.properties import StringProperty
-from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem, OneLineListItem, TwoLineAvatarListItem
+from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem, MDList, OneLineListItem, TwoLineAvatarListItem, BaseListItem
 from kivymd.icon_definitions import md_icons
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelTwoLine
+from kivymd import images_path
 
 
 class ListItemWithCheckbox(OneLineAvatarIconListItem):
@@ -13,7 +17,9 @@ class ListItemWithCheckbox(OneLineAvatarIconListItem):
 
     icon = StringProperty("android")
 
-
+class Content(MDBoxLayout):
+    text = StringProperty("android")
+    pass
 
 class Tab(MDFloatLayout, MDTabsBase):
     '''Class implementing content for a tab.'''
@@ -25,16 +31,29 @@ class MenuGeneratorApp(MDApp):
             day = sdate + timedelta(days=i)
             day_title = "\n{}, {} {}".format(day.strftime("%A"), day.day, day.strftime("%b"))
             self.root.ids.tabs.add_widget(Tab(title=day_title))
-        icons = list(md_icons.keys())
         tabs = self.root.ids.tabs.get_tab_list()
         for tab in tabs:
             for i in range(3):
                 tab.tab.ids.scroll.add_widget(
-                    OneLineListItem(text=f"Meal {i+1}")
+                    OneLineListItem(text=f"Meal {i+1} on day {tab.tab.title}")
                 )
-                tab.tab.ids.scroll.add_widget(
-                    TwoLineAvatarListItem(text=f"Recipe {i+1}")
+                # tab.tab.ids.scroll.add_widget(
+                #     TwoLineAvatarListItem(text=f"Recipe {i+1}")
+                # )
+                panel = MDExpansionPanel(
+                    content=Content(text=f"Recipe instructions for recipe {i+1} on day {tab.tab.title}"),
+                    # content=Content(),
+                    icon=f"{images_path}kivymd.png",
+                    panel_cls=MDExpansionPanelTwoLine(
+                        text=f"Recipe {i+1}",
+                        secondary_text="Secondary text here"                        
+                    )
                 )
+                grid = MDGridLayout(cols=1, adaptive_height=True)
+                grid.add_widget(panel)
+                tab.tab.ids.scroll.add_widget(grid
+                )
+                
             
 
     def on_tab_switch(self, instance_tabs, instance_tab, instance_tab_label, tab_text):
